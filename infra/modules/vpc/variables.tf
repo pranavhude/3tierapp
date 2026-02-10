d773070bf3360d0edf25c3150c778445012b29b0
pranavhude <pranavhude20@gmail.com>
2026-02-10 13:50:09 +0530

    Add 3-tier application code

--- infra/modules/vpc/variables.tf
+++ infra/modules/vpc/variables.tf
@@ -1,3 +1,20 @@
-variable "vpc_cidr" {}
-variable "public_subnets" {}
-variable "private_subnets" {}
+#variable "vpc_cidr" {}
+#variable "public_subnets" {}
+#variable "private_subnets" {}
+
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "public_subnets" {
+  type = list(string)
+}
+
+variable "private_subnets" {
+  type = list(string)
+}
+
+variable "azs" {
+  type = list(string)
+}
