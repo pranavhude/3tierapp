5b4eb37593b3f93853ab48f3e5728600f05c9a3b
pranavhude <pranavhude20@gmail.com>
2026-02-10 13:46:20 +0530

    Add 3-tier application code

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -6,6 +6,7 @@ resource "aws_subnet" "public" {
   count             = length(var.public_subnets)
   vpc_id            = aws_vpc.this.id
   cidr_block        = var.public_subnets[count.index]
+  availability_zone       = var.azs[count.index]
   map_public_ip_on_launch = true
 }
 
@@ -13,4 +14,5 @@ resource "aws_subnet" "private" {
   count      = length(var.private_subnets)
   vpc_id     = aws_vpc.this.id
   cidr_block = var.private_subnets[count.index]
+  availability_zone       = var.azs[count.index]
 }
