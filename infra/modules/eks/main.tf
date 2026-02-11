e6ca1e3383307110b420ee3560e582766382e334
pranavhude <pranavhude20@gmail.com>
2026-02-11 13:47:44 +0530

    fixed iam error

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -18,3 +18,14 @@ resource "aws_eks_node_group" "nodes" {
     max_size     = 3
   }
 }
+
+
+data "tls_certificate" "eks" {
+  url = aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "eks" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+  url             = aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
