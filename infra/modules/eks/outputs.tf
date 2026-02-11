e795cc8c0400ad675444a92e49fc1f784f57902f
pranavhude <pranavhude20@gmail.com>
2026-02-11 14:20:00 +0530

    fixed iam error

--- infra/modules/eks/outputs.tf
+++ infra/modules/eks/outputs.tf
@@ -3,12 +3,12 @@ output "cluster_name" {
 }
 
 output "oidc_provider_arn" {
-  value = aws_iam_openid_connect_provider.eks.arn
+  value = aws_iam_openid_connect_provider.oidc.arn
 }
 
 output "oidc_provider_url" {
   value = replace(
-    aws_iam_openid_connect_provider.eks.url,
+    aws_iam_openid_connect_provider.oidc.url,
     "https://",
     ""
   )
