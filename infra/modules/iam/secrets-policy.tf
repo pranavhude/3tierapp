98135db80ed9bc245700ce94b12c2a1116b87d2e
Pranav Hude <pranavhude20@gmail.com>
2026-02-12 09:31:49 +0530

    Update secrets-policy.tf

--- infra/modules/iam/secrets-policy.tf
+++ infra/modules/iam/secrets-policy.tf
@@ -1,15 +1,1 @@
-resource "aws_iam_policy" "secrets_manager_access" {
-  name        = "backend-secrets-manager-access"
-  description = "Allow backend to read Secrets Manager secret"
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = ["secretsmanager:GetSecretValue"]
-        Resource = var.secret_arn
-      }
-    ]
-  })
-}
