6bdeeaa582d43543e67a78f840e0e37574095ee8
pranavhude <pranavhude20@gmail.com>
2026-02-17 21:05:32 +0530

    Updating IRSA

--- infra/main.tf
+++ infra/main.tf
@@ -7,6 +7,7 @@ module "vpc" {
 
 module "iam" {
   source = "./modules/iam"
+  oidc_provider_arn = module.eks.oidc_provider_arn
   oidc_provider_url = module.eks.oidc_provider_url
 }
 
--- infra/modules/iam/outputs.tf
+++ infra/modules/iam/outputs.tf
@@ -5,3 +5,7 @@ output "cluster_role_arn" {
 output "node_role_arn" {
   value = aws_iam_role.eks_node.arn
 }
+
+output "backend_irsa_role_arn" {
+  value = aws_iam_role.backend_irsa.arn
+}
