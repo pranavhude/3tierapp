a68b49ad263dd596ed184ffd3552dc1f27761186
pranavhude <pranavhude20@gmail.com>
2026-02-17 10:58:22 +0530

    adding OIDC op i eks

--- infra/modules/eks/outputs.tf
+++ infra/modules/eks/outputs.tf
@@ -5,3 +5,15 @@ output "cluster_name" {
 output "cluster_endpoint" {
   value = aws_eks_cluster.this.endpoint
 }
+
+output "oidc_provider_arn" {
+  value = aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
+
+output "oidc_provider_url" {
+  value = replace(
+    aws_eks_cluster.this.identity[0].oidc[0].issuer,
+    "https://",
+    ""
+  )
+}
