a4ecb5f0318e9e41152722b023bde710921549c2
pranavhude <pranavhude20@gmail.com>
2026-02-17 21:13:40 +0530

    Updating IRSA

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -34,3 +34,10 @@ resource "aws_eks_node_group" "nodes" {
   depends_on = [aws_eks_cluster.this]
 }
 
+resource "aws_iam_openid_connect_provider" "oidc" {
+  client_id_list  = ["sts.amazonaws.com"]
+
+  thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da0afd29d8e"]
+
+  url = aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
